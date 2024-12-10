adbb0a78b9008d0ca096f60c27b3eb55f80ab594
Milkyway_Developer <za12hra>
2024-12-11 00:00:01 +0330

    message

---
+++ EX1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter your Number:"))
+if a>10:
+    print("Its bigger than 10")
+if a<=10: 
+    print("Its smaller than 10")
+if a%2==0: 
+    print("Its an even number." )
+if a%2==1: 
+    print("Its an odd number." )
+
+    #بررسی زوج و فرد و بررسی بزرگتر یا کوچک تر از 10 بودن

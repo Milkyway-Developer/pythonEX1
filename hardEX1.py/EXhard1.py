650b5efea079a10a8522b21db7432691bcd66adf
Milkyway_Developer <za12hra>
2024-12-11 17:38:08 +0330

    Upload simple PWA project on Github

---
+++ hardEX1.py/EXhard1.py
@@ -0,0 +1,7 @@
+a = str(input("Enter your text:"))
+if a==a[::-1]:
+    print("true")
+else:
+    print("false")
+    print(a[::-1],"Revers form of",a)
+   
---
+++ hardEX1.py/EXhard2.py
@@ -0,0 +1,5 @@
+import platform
+print (platform.system())
+print (platform.uname())
+
+#ببک وقتی اجرا شد سیستم عامل کاربر را به ان بگوید
